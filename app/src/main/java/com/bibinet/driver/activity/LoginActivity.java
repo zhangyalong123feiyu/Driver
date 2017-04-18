ffd1d287b6f42f06c1c16c6bfc44505603db9f9c
coder-zhangyalong <409812937@qq.com>
2017-04-18 15:21:13 +0800

    首次提交

--- app/src/main/java/com/bibinet/driver/activity/LoginActivity.java
+++ app/src/main/java/com/bibinet/driver/activity/LoginActivity.java
@@ -14,6 +14,10 @@ public class LoginActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initView();
+    }
+
+    private void initView() {
     }
 
 
